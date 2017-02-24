990685b6271f66a89145ff7891a15c011b1de319
Rob <robflettcc@gmail.com>
2017-02-24 14:38:22 +0000

    Seeds corrected - name field blank now

--- db/shelter.sql
+++ db/shelter.sql
@@ -2,15 +2,15 @@ DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS animals; 
 
 CREATE TABLE owners(
-  owner_id SERIAL4 PRIMARY KEY,
+  id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
   -- pets_adopted VARCHAR(255)
   -- funds to be added ---
 );
 
 CREATE TABLE animals(
-  animal_id SERIAL4 PRIMARY KEY,
-  owner_id SERIAL4 REFERENCES owners(owner_id),
+  id SERIAL4 PRIMARY KEY,
+  -- owner_id SERIAL4 REFERENCES owners(id),
   name VARCHAR(255),
   type VARCHAR(255),
   breed VARCHAR(255),
